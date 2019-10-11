e296465acb2a7846dccb72b95059975a5a071739
James Buncle <jbuncle@hotmail.com>
2019-10-11 22:51:36 +0100

    Updted phan to run without dev dependencies

--- .phan/config.php
+++ .phan/config.php
@@ -15,9 +15,10 @@ return [
     "target_php_version" => '7.1',
     'directory_list' => [
         'src',
+        'vendor',
     ],
     "exclude_analysis_directory_list" => [
-        'vendor/'
+        'vendor',
     ],
     'plugins' => [
         'AlwaysReturnPlugin',
